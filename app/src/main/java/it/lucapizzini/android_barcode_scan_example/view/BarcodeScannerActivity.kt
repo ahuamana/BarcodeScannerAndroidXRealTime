eaf355a75d35ac9227888b3845c60a12361908c3
Antony Nicolas Huaman Alikhan <73962071@continental.edu.pe>
2023-02-28 16:05:21 -0500

    first commit

---
+++ app/src/main/java/it/lucapizzini/android_barcode_scan_example/view/BarcodeScannerActivity.kt
@@ -0,0 +1,150 @@
+package it.lucapizzini.android_barcode_scan_example.view
+
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import it.lucapizzini.android_barcode_scan_example.barcode.QrCodeAnalyzer
+import it.lucapizzini.android_barcode_scan_example.databinding.ActivityBarcodeScannerBinding
+import java.lang.IllegalArgumentException
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+class BarcodeScannerActivity : AppCompatActivity() {
+
+    private lateinit var cameraExecutor: ExecutorService
+    private lateinit var barcodeBoxView: BarcodeBoxView
+    private lateinit var binding: ActivityBarcodeScannerBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityBarcodeScannerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
+
+        barcodeBoxView = BarcodeBoxView(this)
+        addContentView(barcodeBoxView, ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT))
+
+        checkCameraPermission()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        cameraExecutor.shutdown()
+    }
+
+    /**
+     * This function is executed once the user has granted or denied the missing permission
+     */
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        checkIfCameraPermissionIsGranted()
+    }
+
+    /**
+     * This function is responsible to request the required CAMERA permission
+     */
+    private fun checkCameraPermission() {
+        try {
+            val requiredPermissions = arrayOf(Manifest.permission.CAMERA)
+             ActivityCompat.requestPermissions(this, requiredPermissions, 0)
+        } catch (e: IllegalArgumentException) {
+            checkIfCameraPermissionIsGranted()
+        }
+    }
+
+    /**
+     * This function will check if the CAMERA permission has been granted.
+     * If so, it will call the function responsible to initialize the camera preview.
+     * Otherwise, it will raise an alert.
+     */
+    private fun checkIfCameraPermissionIsGranted() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            // Permission granted: start the preview
+            startCamera()
+        } else {
+            // Permission denied
+            MaterialAlertDialogBuilder(this)
+                .setTitle("Permission required")
+                .setMessage("This application needs to access the camera to process barcodes")
+                .setPositiveButton("Ok") { _, _ ->
+                    // Keep asking for permission until granted
+                    checkCameraPermission()
+                }
+                .setCancelable(false)
+                .create()
+                .apply {
+                    setCanceledOnTouchOutside(false)
+                    show()
+                }
+        }
+    }
+
+    /**
+     * This function is responsible for the setup of the camera preview and the image analyzer.
+     */
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            val cameraProvider = cameraProviderFuture.get()
+
+            // Preview
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.previewView.surfaceProvider)
+                }
+
+            // Image analyzer
+            val imageAnalyzer = ImageAnalysis
+                .Builder()
+                .setOutputImageFormat(ImageAnalysis.OUTPUT_IMAGE_FORMAT_YUV_420_888)
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .build()
+                .also {
+                    it.setAnalyzer(
+                        cameraExecutor,
+                        QrCodeAnalyzer(
+                            this,
+                            barcodeBoxView,
+                            binding.previewView.width.toFloat(),
+                            binding.previewView.height.toFloat()
+                        )
+                    )
+                }
+
+            // Select back camera as a default
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+
+                // Bind use cases to camera
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview, imageAnalyzer
+                )
+
+            } catch (exc: Exception) {
+                print("Use case binding failed")
+                exc.printStackTrace()
+            }
+        }, ContextCompat.getMainExecutor(this))
+    }
+}
